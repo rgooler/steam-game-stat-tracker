1cb507092a8d6d686a95981a9826ae330a2d485b
Ryan Gooler <ryan.gooler@gmail.com>
2019-03-31 14:51:58 -0500

    Clean up some pep issues and unneeded files

--- steam_game_stat_tracker.py
+++ steam_game_stat_tracker.py
@@ -5,13 +5,15 @@ from steamgamestattracker.steamapi import SteamAPI
 import json
 import sys
 
+
 def main():
     steam = SteamAPI(apikey="B3E1C11C73B61B18C4A340CB1B464115")
-    #jippen = "76561197993843660"
+    # jippen = "76561197993843660"
     # games = steam.GetOwnedGames(jippen)
     # game = games['response']['games'][0]['appid']
     # todo: for loop this
-    # print(json.dumps(steam.GetSchemaForGame(218620), sort_keys=True, indent=2))
+    # schema = steam.GetSchemaForGame(218620)
+    # print(json.dumps(schema, sort_keys=True, indent=2))
 
     print(json.dumps(steam.GetSchemaForGame(218620), sort_keys=True, indent=2))
     return 0
